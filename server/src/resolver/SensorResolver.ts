34fc62170c780688b74cdc4d697043c8d6bcba98
thomsva <thomsva@gmail.com>
2022-03-30 13:29:52 +0300

    Sensor crud functionality

--- server/src/resolver/SensorResolver.ts
+++ server/src/resolver/SensorResolver.ts
@@ -1,13 +1,35 @@
 //import { Context } from 'apollo-server-core';
-import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
-import { AppDataSource } from '../data-source';
+import {
+  Arg,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver
+} from 'type-graphql';
 import Sensor from '../entity/Sensor';
-import Book from '../entity/Sensor';
 
-const sensorRepository = AppDataSource.getRepository(Book);
-
-@InputType({ description: 'New sensor data' })
+@InputType({ description: 'Data for new sensor' })
 class AddSensorInput implements Partial<Sensor> {
+  @Field()
+  name!: string;
+
+  @Field()
+  type!: string;
+
+  @Field()
+  description!: string;
+
+  @Field({ nullable: true })
+  location?: string;
+}
+
+@InputType({ description: 'Data for editing sensor' })
+class editSensorInput implements Partial<Sensor> {
+  @Field()
+  id!: number;
+
   @Field()
   name!: string;
 
@@ -27,11 +49,32 @@ export class SensorResolver {
   async addSensor(@Arg('data') newSensorData: AddSensorInput): Promise<Sensor> {
     const sensor = Sensor.create(newSensorData);
     await sensor.save();
+    console.log('sensor saved:', sensor);
+    return sensor;
+  }
+
+  @Mutation(() => Sensor)
+  async editSensor(
+    @Arg('data') editSensorData: editSensorInput
+  ): Promise<Sensor | null> {
+    const sensor = Sensor.create(editSensorData);
+    await sensor.save();
     return sensor;
   }
 
   @Query(() => [Sensor], { description: 'Get all the sensors.' })
-  async books(): Promise<Book[]> {
-    return await sensorRepository.find();
+  async GetSensors(): Promise<Sensor[]> {
+    return await Sensor.find();
+  }
+
+  @Query(() => Sensor)
+  async getSensor(@Arg('id', () => Int) id: number): Promise<Sensor | null> {
+    return Sensor.findOneBy({ id: id });
+  }
+
+  @Mutation(() => Boolean)
+  async deleteSensor(@Arg('id', () => Int) id: number): Promise<boolean> {
+    await Sensor.delete({ id: id });
+    return true;
   }
 }
