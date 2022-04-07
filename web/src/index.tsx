d33e3857148e9fe3915e13e4877aaed6af3b1614
thomsva <thomsva@gmail.com>
2022-04-07 19:24:08 +0300

    Implementing Material UI 5

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,6 +7,45 @@ import {
   InMemoryCache,
   useQuery
 } from '@apollo/client';
+import {
+  Button,
+  createTheme,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  ThemeProvider,
+  Typography
+} from '@mui/material';
+import HourglassBottomTwoToneIcon from '@mui/icons-material/HourglassBottomTwoTone';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5'
+    },
+    secondary: {
+      main: '#f50057'
+    }
+  }
+});
 
 const USERS_QUERY = gql`
   query Users {
@@ -34,27 +73,36 @@ const UserList = () => {
   const { loading, data } = useQuery<UsersData>(USERS_QUERY);
   return (
     <div>
-      <h3>List of users</h3>
+      <Typography variant="h3" gutterBottom component="div">
+        List of users
+      </Typography>
       {loading ? (
-        <p>Loading ...</p>
+        <HourglassBottomTwoToneIcon />
       ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nickname</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.users.map((u) => (
-                <tr key={u.id}>
-                  <td>{u.id}</td>
-                  <td>{u.nickname}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Nickname</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data &&
+                data.users.map((u) => (
+                  <TableRow
+                    key={u.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {u.id}
+                    </TableCell>
+                    <TableCell align="right">{u.nickname}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </div>
   );
@@ -70,14 +118,20 @@ interface WelcomeProps {
 }
 
 const Welcome = (props: WelcomeProps) => {
-  return <h1>Hello {props.name}!</h1>;
+  return (
+    <Button variant="contained" color="primary">
+      Hello {props.name}!
+    </Button>
+  );
 };
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Welcome name="world" />
-      <UserList />
+      <ThemeProvider theme={theme}>
+        <Welcome name="world" />
+        <UserList />
+      </ThemeProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
