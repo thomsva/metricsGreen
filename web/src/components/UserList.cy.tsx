e621d1f026dfde938887bba9c83bdde3772082cf
Thomas S <thomsva@gmail.com>
2023-02-23 13:48:09 +0200

    Removed Cypress

--- web/src/components/UserList.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import UserList from './UserList'
-
-describe('<UserList />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<UserList />)
-  })
-})
