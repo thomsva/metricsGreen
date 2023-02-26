b1af07bce74af5c047896ed5da156b3974e41d61
Thomas S <thomsva@gmail.com>
2023-02-26 19:22:33 +0200

    Edit placeholder test

--- app-test/cypress/e2e/login.cy.js
+++ app-test/cypress/e2e/login.cy.js
@@ -1,11 +1,5 @@
 describe('pages can be loaded', () => {
   it('loads front page', () => {
     cy.visit('/')
-    cy.contains('Hello')
-  })
-
-  it('loads register page', () => {
-    cy.visit('/register')
-    cy.contains('world')
   })
 })
