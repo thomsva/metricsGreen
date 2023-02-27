8df32d722b82313e3bc34abbaca3110664969e89
thomsva <thomsva@gmail.com>
2023-02-27 09:22:32 +0200

    Prettier and eslint

--- app-test/cypress.config.js
+++ app-test/cypress.config.js
@@ -1,13 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
-
-
--- app-test/cypress/e2e/login.cy.js
+++ app-test/cypress/e2e/login.cy.js
@@ -1,13 +1,11 @@
 describe('users', () => {
   it('can log in from the user menu on the app bar', () => {
-    cy.visit('/')
-    cy.get('[data-testid="guestUserIcon"]').click()
-    cy.get('[data-testid="userMenuLogin"]').click()
-    cy.get('[data-testid="userName').clear().type('don')
-    cy.get('[data-testid="password').clear().type('pwd')
-    cy.get('[data-testid="submitLogin').click()
-    cy.get('[data-testid="accountUserIcon"]').should('be.visible')
-  })
-  
-})
-
+    cy.visit('/');
+    cy.get('[data-testid="guestUserIcon"]').click();
+    cy.get('[data-testid="userMenuLogin"]').click();
+    cy.get('[data-testid="userName').clear().type('don');
+    cy.get('[data-testid="password').clear().type('pwd');
+    cy.get('[data-testid="submitLogin').click();
+    cy.get('[data-testid="accountUserIcon"]').should('be.visible');
+  });
+});
